0cb284be496d15addfeebd395b0988da7c864880
daha <zhengjin.he@hipac.cn>
2021-03-07 10:21:34 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
  * @Author: daha
  * @Description: 
  * @Date: 1985-10-26 16:15:00
- * @LastEditTime: 2021-03-05 16:31:03
+ * @LastEditTime: 2021-03-07 10:21:18
  * @LastEditors: daha
  */
 import React, { useEffect, useState } from 'react';
@@ -13,17 +13,6 @@ import './App.less';
 function App() {
 	const [userName, setUserName] = useState<string>('');
 
-	// useEffect(() => {
-	// 	const IDBRequest = indexedDB.open('my-chat', 4);
-	// 	IDBRequest.onupgradeneeded = (event) => {
-	// 		let db = event?.target?.addEventListener('');
-	// 		db.createObjectStore(
-	// 			'message',
-	// 			{ autoIncrement: true }
-	// 		);
-	// 		console.log(db);
-	// 	}
-	// }, [])
 	const login = (userName: string) => {
 		!!userName && setUserName(userName);
 	};
